ab154107b2f0a6c45377f29e9b2db395729e7bd6
Flex58 <164533598+Flex58@users.noreply.github.com>
2024-04-08 14:18:30 +0200

    Add playGame function and finish RPS project

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
+// 0 = Rock, 1 = Paper, 2 = Scissors
+// 0 = tie, 1 = win, 2 = loss
+
+playGame();
+
 //gets the computers choice of either rock paper or scissors
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3);
@@ -32,58 +37,62 @@ function playRound(userChoice, computerChoice){
             case "Rock":
                 console.log("Rock ties against Rock, you tie");
                 result = 0;
-                break;
+                return result;
             case "Paper":
                 console.log("Rock gets beaten by Paper, you lose");
                 result = 2;
-                break;
+                return result;
             case "Scissors":
                 console.log("Rock beats Scissors, you win");
-                result = 1;
-                break;
+                return result = 1;
         }
-        console.log(result);
+        return result;
     }
     else if (userChoice == "Paper"){
         switch (computerChoice){
             case "Rock":
                 console.log("Paper beats Rock, you win");
-                result = 1;
-                break;
+                return result = 1;
             case "Paper":
                 console.log("Paper ties against Paper, you tie");
-                result = 0;
-                break;
+                return result = 0;
             case "Scissors":
                 console.log("Paper gets beaten by Scissors, you lose");
-                result = 2;
-                break;
+                return result = 2;
         }
-        console.log(result);
     }
     else if (userChoice == "Scissors"){
         switch (computerChoice){
             case "Rock":
                 console.log("Scissors gets beaten by Rock, you lose");
-                result = 2;
-                break;
+                return result = 2;
             case "Paper":
                 console.log("Scissors beats Paper, you win");
-                result = 1;
-                break;
+                return result = 1;
             case "Scissors":
                 console.log("Scissors ties against Scissors, you tie");
-                result = 0;
-                break;
+                return result = 0;
         }
-        console.log(result);
     }
-    else{
-        playRound(getUserChoice(), getComputerChoice());
-    }
-    return result;
 }
-playRound(getUserChoice(),getComputerChoice());
+//plays a full match bo3 of rps
+function playGame(){
+    let userWins = 0;
+    let computerWins = 0;
+    while(userWins < 3 && computerWins < 3){
+        let result = playRound(getUserChoice(), getComputerChoice());
 
-// 0 = Rock, 1 = Paper, 2 = Scissors
-// 0 = tie, 1 = win, 2 = loss
+        if (result == 1){
+            userWins++;
+        }
+        else if (result == 2){
+            computerWins++;
+        }
+    }
+    if (userWins == 3){
+        console.log("You Win!");
+    }
+    else if (computerWins == 3){
+        console.log("You Lose!");
+    }
+}
