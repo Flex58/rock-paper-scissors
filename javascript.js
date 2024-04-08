2f473642b2608f0f75930eed7630cdbf513ba158
Flex58 <164533598+Flex58@users.noreply.github.com>
2024-04-08 13:34:49 +0200

    Add functioning playRound function

--- javascript.js
+++ javascript.js
@@ -1,4 +1,3 @@
-
 //gets the computers choice of either rock paper or scissors
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3);
@@ -14,7 +13,7 @@ function getComputerChoice(){
     }
     return choice;
 }
-
+//gets the users choice 
 function getUserChoice(){
    let choice = prompt("Rock, Paper or Scissors?");
 
@@ -25,5 +24,66 @@ function getUserChoice(){
    choiceLower = choiceLower.toLowerCase();
    return choiceUpper + choiceLower;
 }
-console.log(getUserChoice());
+//plays a round of rps by comparing user input to computerChoice 
+function playRound(userChoice, computerChoice){
+    let result;
+    if (userChoice == "Rock"){
+        switch (computerChoice){
+            case "Rock":
+                console.log("Rock ties against Rock, you tie");
+                result = 0;
+                break;
+            case "Paper":
+                console.log("Rock gets beaten by Paper, you lose");
+                result = 2;
+                break;
+            case "Scissors":
+                console.log("Rock beats Scissors, you win");
+                result = 1;
+                break;
+        }
+        console.log(result);
+    }
+    else if (userChoice == "Paper"){
+        switch (computerChoice){
+            case "Rock":
+                console.log("Paper beats Rock, you win");
+                result = 1;
+                break;
+            case "Paper":
+                console.log("Paper ties against Paper, you tie");
+                result = 0;
+                break;
+            case "Scissors":
+                console.log("Paper gets beaten by Scissors, you lose");
+                result = 2;
+                break;
+        }
+        console.log(result);
+    }
+    else if (userChoice == "Scissors"){
+        switch (computerChoice){
+            case "Rock":
+                console.log("Scissors gets beaten by Rock, you lose");
+                result = 2;
+                break;
+            case "Paper":
+                console.log("Scissors beats Paper, you win");
+                result = 1;
+                break;
+            case "Scissors":
+                console.log("Scissors ties against Scissors, you tie");
+                result = 0;
+                break;
+        }
+        console.log(result);
+    }
+    else{
+        playRound(getUserChoice(), getComputerChoice());
+    }
+    return result;
+}
+playRound(getUserChoice(),getComputerChoice());
+
 // 0 = Rock, 1 = Paper, 2 = Scissors
+// 0 = tie, 1 = win, 2 = loss
